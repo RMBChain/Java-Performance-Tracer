1c2cf8cf0aa4537e688d407c7d73d435923ac0d3
spooner <xwhxuxwh@cn.ibm.com>
2020-04-29 00:04:22 +0800

    update interval for every test

--- jds-core-tester/entrypoint.sh
+++ jds-core-tester/entrypoint.sh
@@ -3,5 +3,5 @@
 while true                                               
   do                                                     
     /usr/local/openjdk-8/bin/java -javaagent:/home/jds-core-1.0.8-SNAPSHOT.jar=/home/workspace -cp jds-core-tester-1.0.8-SNAPSHOT.jar com.thirdpart.jds.test.JDS_CoreTester_Application
-    sleep 60                                 
+    sleep 120                                 
   done                                        
