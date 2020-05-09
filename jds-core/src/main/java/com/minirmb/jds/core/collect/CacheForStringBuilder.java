15c0d67c11812e67e29828bbf99769ca10a17736
67099059@qq.com <Qw23.*54We]ioP>
2020-05-10 00:48:34 +0800

    Add cache for StringBuilder

---
+++ jds-core/src/main/java/com/minirmb/jds/core/collect/CacheForStringBuilder.java
@@ -0,0 +1,28 @@
+package com.minirmb.jds.core.collect;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 使用CacheForStringBuffer是为了防止创建太多的StringBuilder对象。减轻gc的压力
+ *
+ *
+ */
+public class CacheForStringBuilder {
+    private static Queue<StringBuilder> cache = new ConcurrentLinkedQueue<>();
+
+    private static ReentrantLock lock = new ReentrantLock() ;
+    public static void put(StringBuilder ele){
+        ele.delete(0,ele.length());
+        cache.offer(ele);
+    }
+
+    public static StringBuilder get(){
+        //StringBuilder result = null;
+        lock.lock();
+        StringBuilder result = cache.size() > 0 ? cache.poll() : new StringBuilder();
+        lock.unlock();
+        return result;
+    }
+}
